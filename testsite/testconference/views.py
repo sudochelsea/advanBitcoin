3e9eee576d81f0e31f13769bac95beac012ba5bb
chelsea <chelseakowouvi@gmail>
2022-08-26 05:43:04 +0000

    changes to test blog

--- testsite/testconference/views.py
+++ testsite/testconference/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+from django.conf import settings
+from django.shortcuts import render
+import requests 
+from requests import RequestException
+
+GHOST_KEY = settings.GHOST_KEY
+BASE_URL = settings.BASE_URL
+VERSION = settings.VERSION
+LIMIT = settings.LIMIT
+
+# i couldn't really figure how to do this, i just followed through on the old codebase
+
+def index(request):
+    page = request.GET.get('page_id', 1)
+    posts = requests.get(
+        f'''{BASE_URL}/{VERSION}/content/posts/?key={GHOST_KEY}&include=authors,tags&limit={LIMIT}&page={page}''')
+    return render(request, 'blog/blog_homepage.html', {
+            'posts': posts.json()['posts'],
+            'pagination': posts.json()['meta']['pagination'],
+        })
+   
 
 
 # def index(request):
@@ -25,3 +45,6 @@ def speakers(request):
 
 def sponsors(request):
     return render(request, "sponsors.html")
+
+def blog(request):
+    return render(request, "blog.html"),
